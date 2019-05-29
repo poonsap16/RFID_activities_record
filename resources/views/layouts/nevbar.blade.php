9b78f4bc706ab70db429923a6adf3af8f5b47921
poonsap16 <kengp16@hotmail.com>
2019-05-29 17:15:27 +0700

    edit git push

--- resources/views/layouts/nevbar.blade.php
+++ resources/views/layouts/nevbar.blade.php
@@ -3,20 +3,20 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-danger sticky-top">
 
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#rfid">RFID Activities Record</a>
+      <a class="navbar-brand js-scroll-trigger" href="/home">RFID Activities Record</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#Home">หน้าแรก</a>
+            <a class="nav-link js-scroll-trigger" href="/home">หน้าแรก</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#about">ปฏิทินกิจกรรม</a>
+            <a class="nav-link js-scroll-trigger" href="/calendar">ปฏิทินกิจกรรม</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#contact">รายงาน</a>
+            <a class="nav-link js-scroll-trigger" href="/report">รายงาน</a>
           </li>
           <li class="nav-item">
             <button type="button" class="btn btn-dark">เข้าสู่ระบบ</button>
--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -1,24 +1,38 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<style type="text/css">
-		$(document).ready(function() {
+@extends('layouts.app')
 
-    // page is now ready, initialize the calendar..
-    $('#calendar').fullCalendar({
-        weekends: false,
-        dayClick: function() {
-          alert('a day has been clicked!');
-        },
-        defaultView: 'agendaWeek'
-        
-    });
-	</style>
-</head>
-<body>
- <div id='calendar'></div>
-</body>
-</html>
+@section('title','ตารางกิจกรรม ภาวิชาอายุรศาสตร์')
+
+@include('layouts.nevbar')
+
+@section('head','ปฏิทินกิจกรรม')
+
+@section('content')
+<table class="table table-hover" align="center">
+  <thead>
+    <tr>
+      <th scope="col">วันที่</th>
+      <th scope="col">ชื่อกิจกรรม</th>
+      <th scope="col">สถานที่</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>27 พฤษภาคม 2562</td>
+      <td>Morning Report</td>
+      <td>ห้องประชุมจงจินต์ ตึกธนาคารไทยพาณิชย์ ชั้น 1</td>
+    </tr>
+    <tr>
+      <td>28 พฤษภาคม 2562</td>
+      <td>Morning Report</td>
+      <td>ห้องประชุมจงจินต์ ตึกธนาคารไทยพาณิชย์ ชั้น 1</td>
+    </tr>
+    <tr>
+      <td>29 พฤษภาคม 2562</td>
+      <td>Medical Conferance</td>
+      <td>ห้องประชุมจงจินต์ ตึกอัษฎางค์ ชั้น 4</td>
+    </tr>
+  </tbody>
+</table>
+@endsection
 
 
